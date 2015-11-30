fea77b85bee24ebd1005aa9e4c5751abc221cc0a
vaferrere <gadoy512@gmail.com>
2015-11-30 10:57:41 +0100

    Grille fonctionnelle

---
+++ PROJECT Roi Dadidou/Assets/Scripts/GridScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GridScript : MonoBehaviour {
+	public Object go;
+	public int width;
+	public int height;
+
+	void Start()
+	{
+		for(int i = 0;i<height;i++)
+		{
+			for(int j = 0;j<width;j++)
+			{
+				go = Instantiate(go,new Vector3(i, j, 0),Quaternion.identity);
+				go.name="Tile("+i+","+j+")";
+			}
+		}
+	}
+}
