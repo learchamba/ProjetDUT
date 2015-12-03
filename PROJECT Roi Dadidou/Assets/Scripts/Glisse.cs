87fa5b5619d1c8de09f2b6e8235a297cccda6d50
vaferrere <gadoy512@gmail.com>
2015-12-03 19:39:16 +0100

    Fix problème tremblement
    Résolution des problèmes de tremblements qu'il pouvait y avoir entre la
    collision de deux blocs ou du joueur et d'un  bloc.

--- PROJECT Roi Dadidou/Assets/Scripts/Glisse.cs
+++ PROJECT Roi Dadidou/Assets/Scripts/Glisse.cs
@@ -5,6 +5,10 @@ public class Glisse : MonoBehaviour {
 	Vector3 pos;                                // For movement
 	float speed = 4.0f; 
 	bool moving = false;
+	bool left = false;
+	bool right = false;
+	bool up = false;
+	bool down = false;
 	string direction = null;
 	PlayerWalk player;
 	// Use this for initialization
@@ -14,24 +18,31 @@ public class Glisse : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (moving == true) {
-			if (direction == "right")
+			if (direction == "right" && !right )
 				pos.x++;
-			if (direction == "left")
+			if (direction == "left" && !left)
 				pos.x--;
-			if (direction == "up")
+			if (direction == "up" && !up)
 				pos.y++;
-			if (direction == "down")
+			if (direction == "down" && !down)
 				pos.y--;
 
 			transform.position = Vector3.MoveTowards (transform.position, pos, Time.deltaTime * speed);
-		}
 
 	}
 
 	void OnCollisionExit2D(Collision2D collision){
 
-		if (collision.gameObject.tag == "Player") {
+		if (Mathf.Round(collision.gameObject.transform.position.x) > Mathf.Round(transform.position.x) )
+			right = false;
+		if (Mathf.Round(collision.gameObject.transform.position.x) < Mathf.Round(transform.position.x))
+			left = false;
+		if (Mathf.Round(collision.gameObject.transform.position.y) > Mathf.Round(transform.position.y) )
+			up = false;
+		if (Mathf.Round(collision.gameObject.transform.position.y) < Mathf.Round(transform.position.y))
+			down =false;
+
+		if (collision.gameObject.tag == "Player" ) {
 			moving = true;
 			player = collision.gameObject.GetComponent("PlayerWalk") as PlayerWalk ;
 			direction = player.getLastInput();
@@ -44,14 +55,20 @@ public class Glisse : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
-		moving = false;
+
+		if (Mathf.Round(collision.gameObject.transform.position.x) > Mathf.Round(transform.position.x) )
+			right = true;
+		if (Mathf.Round(collision.gameObject.transform.position.x) < Mathf.Round(transform.position.x))
+			left = true;
+		if (Mathf.Round(collision.gameObject.transform.position.y) > Mathf.Round(transform.position.y) )
+			up = true;
+		if (Mathf.Round(collision.gameObject.transform.position.y) < Mathf.Round(transform.position.y))
+			down = true;
 		pos.x = Mathf.Round (transform.position.x);
 		pos.y = Mathf.Round (transform.position.y);
 		transform.position = pos;
 	}
 
-	void OnCollisionStay2D(Collision2D collision){
 
-	}
 
 }
--- PROJECT Roi Dadidou/Assets/Scripts/GridScript.cs
+++ PROJECT Roi Dadidou/Assets/Scripts/GridScript.cs
@@ -2,11 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 
+public class Column {
+	public GameObject[] tile ;
+}
+
+public class Grid {
+	public Column[] grid;
+}
+
 public class GridScript : MonoBehaviour {
 	public Object ground;
 	public Object wall;
 	public int width;
 	public int height;
+	Grid grid;
 
 	void Start()
 	{
@@ -17,10 +26,13 @@ public class GridScript : MonoBehaviour {
 				if (i == 0 || j == 0 || i == width - 1 || j == height - 1){
 					wall = Instantiate(wall,new Vector3(i, j, 0),Quaternion.identity);
 					wall.name="Tile("+i+","+j+")";
+					//grid.grid[i].tile[j].AddComponent(wall);
+
 				}
 				else{
 					ground = Instantiate(ground,new Vector3(i, j, 0),Quaternion.identity);
 					ground.name="Tile("+i+","+j+")";
+					//grid.grid[i].tile[j].AddComponent(ground);
 				}
 			}
 		}
