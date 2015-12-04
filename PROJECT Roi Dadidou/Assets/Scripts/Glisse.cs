4c020131c5a7fe28a81d0fa2325b38994a4d7e60
vaferrere <gadoy512@gmail.com>
2015-12-04 15:03:22 +0100

    Factorisation du code dans glisse.cs

--- PROJECT Roi Dadidou/Assets/Scripts/Glisse.cs
+++ PROJECT Roi Dadidou/Assets/Scripts/Glisse.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class Glisse : MonoBehaviour {
 	Vector3 pos;                                // For movement
 	float speed = 4.0f; 
-	bool moving = false;
 	bool left = false;
 	bool right = false;
 	bool up = false;
 	bool down = false;
 	string direction = null;
 	PlayerWalk player;
+	Vector2 collisionposition;
+	Vector2 position;
 	// Use this for initialization
 	void Start () {
 		pos = transform.position;
@@ -32,60 +33,59 @@ public class Glisse : MonoBehaviour {
 	}
 
 	void OnCollisionExit2D(Collision2D collision){
-
-		if (Mathf.Round(collision.gameObject.transform.position.x) > Mathf.Round(transform.position.x) )
-			right = false;
-		if (Mathf.Round(collision.gameObject.transform.position.x) < Mathf.Round(transform.position.x))
-			left = false;
-		if (Mathf.Round(collision.gameObject.transform.position.y) > Mathf.Round(transform.position.y) )
-			up = false;
-		if (Mathf.Round(collision.gameObject.transform.position.y) < Mathf.Round(transform.position.y))
-			down =false;
+		fonctCollision(false,collision);
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
-
-		if ((Mathf.Round(collision.gameObject.transform.position.x) > Mathf.Round(transform.position.x)) && (Mathf.Round(collision.gameObject.transform.position.y) == Mathf.Round(transform.position.y)) ){
-			right = true;
-		}
-		if ((Mathf.Round(collision.gameObject.transform.position.x) < Mathf.Round(transform.position.x)) && (Mathf.Round(collision.gameObject.transform.position.y) == Mathf.Round(transform.position.y)) ){
-			left = true;
-		}
-		if ((Mathf.Round(collision.gameObject.transform.position.y) > Mathf.Round(transform.position.y)) && (Mathf.Round(collision.gameObject.transform.position.x) == Mathf.Round(transform.position.x)) ){
-			up = true;
-		}
-		if ((Mathf.Round(collision.gameObject.transform.position.y) < Mathf.Round(transform.position.y)) && (Mathf.Round(collision.gameObject.transform.position.x) == Mathf.Round(transform.position.x)) ){
-			down = true;
-		}
+		fonctCollision(true,collision);
 
 		if (collision.gameObject.tag == "Player" ) {
 			player = collision.gameObject.GetComponent("PlayerWalk") as PlayerWalk ;
 			direction = player.getLastInput();
 		}else {
-			if ((Mathf.Round(collision.gameObject.transform.position.x) > Mathf.Round(transform.position.x)) && (Mathf.Round(collision.gameObject.transform.position.y) == Mathf.Round(transform.position.y)) ){
+			if ((collisionposition.x > position.x) && (collisionposition.y == position.y)){
 				if (direction == "right"){
 					direction = null;
 					pos.x = Mathf.Round (transform.position.x);
 				}
 			}
-			if ((Mathf.Round(collision.gameObject.transform.position.x) < Mathf.Round(transform.position.x)) && (Mathf.Round(collision.gameObject.transform.position.y) == Mathf.Round(transform.position.y)) ){
+			if ((collisionposition.x < position.x) && (collisionposition.y == position.y)){
 				if (direction == "left"){
 					direction = null;
 					pos.x = Mathf.Round (transform.position.x);
 				}
 			}
-			if ((Mathf.Round(collision.gameObject.transform.position.y) > Mathf.Round(transform.position.y)) && (Mathf.Round(collision.gameObject.transform.position.x) == Mathf.Round(transform.position.x)) ){
+			if ((collisionposition.y > position.y) && (collisionposition.x == position.x)){
 				if (direction == "up"){
 					direction = null;
 					pos.y = Mathf.Round (transform.position.y);
 				}
 			}
-			if ((Mathf.Round(collision.gameObject.transform.position.y) < Mathf.Round(transform.position.y)) && (Mathf.Round(collision.gameObject.transform.position.x) == Mathf.Round(transform.position.x)) ){
+			if ((collisionposition.y < position.y) && (collisionposition.x == position.x)){
 				if (direction == "down"){
 					direction = null;
 					pos.y = Mathf.Round (transform.position.y);
 				}
 			}
+		}
+	}
+
+	public void fonctCollision(bool etat, Collision2D collision){
+		position.x = Mathf.Round(transform.position.x) ;
+		position.y = Mathf.Round(transform.position.y) ;
+		collisionposition.x = Mathf.Round(collision.gameObject.transform.position.x);
+		collisionposition.y = Mathf.Round(collision.gameObject.transform.position.y);
+		if ((collisionposition.x > position.x) && (collisionposition.y == position.y)){
+			right = etat;
+		}
+		if ((collisionposition.x < position.x) && (collisionposition.y == position.y)){
+			left = etat;
+		}
+		if ((collisionposition.y > position.y) && (collisionposition.x == position.x)){
+			up = etat;
+		}
+		if ((collisionposition.y < position.y) && (collisionposition.x == position.x)){
+			down = etat;
 		}
 	}
 
